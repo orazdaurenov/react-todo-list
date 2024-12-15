200fbaf862ae1c56a1d921614ae506ab3c1fa1a6
orazbek <oraz.daurenov@gmail.com>
2024-12-16 00:46:25 +0100

    refactor todos into obj array
    Co-authored-by: TheMcnafaha <TheMcnafaha@users.noreply.github.com>

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -2,40 +2,58 @@ import React, { useState } from "react";
 
 const List = () => {
   //start with a default
+  type TodoObj = {
+    todo: string;
+    isDone: boolean;
+  };
+
+  const newInitialTodos: Array<TodoObj> = [
+    { todo: "First Todo", isDone: false },
+  ];
   let InitialTodos = ["First Todo", "Second Todo", "Third Todo"];
 
   //two way data binding: on change, update the variable and re render
   const [todos, setTodos] = useState(InitialTodos);
   const [newTodo, setNewTodo] = useState(" ");
+  const [neoTodos, setNeoTodos] = useState(newInitialTodos);
+  const [neoTodo, setNeoTodo] = useState<TodoObj>({ todo: "", isDone: false });
   return (
     <div>
       <h1>Todo List</h1>
+      <p>neo stuff</p>
       <label htmlFor="">
         <input
           type="text"
           onChange={(e) => {
-            setNewTodo(e.target.value);
+            // {todo:string, isDone: boolean}
+            // {key:value}
+            const nextTodoObj: TodoObj = {
+              todo: e.target.value,
+              isDone: false,
+            };
+            setNeoTodo(nextTodoObj);
           }}
-          value={newTodo}
+          value={neoTodo.todo}
         />
         <button
           type="submit"
           onClick={() => {
-            setTodos([...todos, newTodo]);
+            setNeoTodos([...neoTodos, neoTodo]);
+
+            setNeoTodo({ todo: " ", isDone: false });
           }}
         >
           SAVE
         </button>
       </label>
-      <p>test: {newTodo}</p>
       <ul>
-        {todos.map((todo, index) => (
+        {neoTodos.map((todo, index) => (
           <div key={index}>
-            <li>{todo}</li>
+            <li>{todo.todo}</li>
             <button
               onClick={() => {
-                const nextTodos = todos.filter((_, i) => i !== index);
-                setTodos(nextTodos);
+                const nextTodos = neoTodos.filter((_, i) => i !== index);
+                setNeoTodos(nextTodos);
               }}
             >
               DELETE
